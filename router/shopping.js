10b7fa1c4d01aa1859778e7d025c9b12cbe31039
smarcat <1308283679@qq.com>
2021-04-22 19:56:52 +0800

    4.22

--- router/shopping.js
+++ router/shopping.js
@@ -3,7 +3,7 @@ const pool = require('../mysql/pool.js');
  const mysql =require('../mysql/pool.js');
  const r =express.Router();
  r.get('/shop',(req,res)=>{
-     pool.query('select*from img_gshop where uid>6 and uid<22',(err,result)=>{
+     pool.query('select * from gshop where uid>6 and uid<22',(err,result)=>{
          if(err) {
              console.log(err)
              return;
